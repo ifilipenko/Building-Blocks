13c64be2fccb90f5e9bb3c960f9f9f3c16f30e05
ifilipenko <filipenko.in@gmail.com>
2011-11-08 02:08:32 +0500

    [+] add link to CommonAssemblyInfo.cs to each projects (except tests) [+] delete some instructions from AssemblyInfo in each projects (except tests) [-] remove assembly product name setting by RakeFile

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -2,7 +2,6 @@ using System.Security;
 using System.Reflection;
 using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("http://timesmart.ru")]
-[assembly: AssemblyProduct("Building blocks")]
 [assembly: AssemblyCopyright("Copyright 2011 Innokentiy Filipenko. All rights reserved.")]
 [assembly: AssemblyVersion("0.1.0.0")]
 
