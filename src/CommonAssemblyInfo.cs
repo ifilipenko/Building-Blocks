0fc684a0b65f26d54ba81ac72ff1d8dc80f19a36
ifilipenko <filipenko.in@gmail.com>
2011-11-08 01:16:46 +0500

    [+] add rakefile with building logic [+] add to all projects target for build nuget package

---
+++ src/CommonAssemblyInfo.cs
@@ -0,0 +1,10 @@
+using System.Security;
+using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyCompany("http://timesmart.ru")]
+[assembly: AssemblyProduct("Building blocks")]
+[assembly: AssemblyCopyright("Copyright 2011 Innokentiy Filipenko. All rights reserved.")]
+[assembly: AssemblyVersion("0.1.0.0")]
+
+[assembly: AllowPartiallyTrustedCallers()]
+
