a1748a6234f2bc6dace5fd6292c5efaa116904b6
ifilipenko <filipenko.in@gmail.com>
2011-11-12 21:12:54 +0500

    [-] remove AllowPartiallyTrustedCallers attribute

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -5,5 +5,3 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright 2011 Innokentiy Filipenko. All rights reserved.")]
 [assembly: AssemblyVersion("0.1.0.0")]
 
-[assembly: AllowPartiallyTrustedCallers()]
-
