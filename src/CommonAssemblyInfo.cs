a0236c57f4c12574a830fd153f13406b71902fa4
ifilipenko <filipenko.in@gmail.com>
2012-09-26 22:08:01 +0600

    fix default assembly info company name

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -2,6 +2,6 @@ using System.Security;
 using System.Reflection;
 using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Building-Blocks")]
-[assembly: AssemblyCopyright("Copyright 2012 Innokentiy Filipenko. All rights reserved.")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyCopyright("Copyright 2011 Innokentiy Filipenko. All rights reserved.")]
+[assembly: AssemblyVersion("0.1.0.0")]
 
