42631f7c25e60ad2b1f2deb4c3eb5f28d140ebb6
Filipenko Innokentiy <filipenko.in@gmail.com>
2013-08-07 16:29:18 +0600

    Add Natural comparer and collection shuffle helpers

---
+++ src/BuildingBlocks.Common/CollectionShuffleHelpers.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace BuildingBlocks.Common
+{
+    public static class CollectionShuffleHelpers
+    {
+        /// <summary>
+        /// Shuffle elements in very good random order. Very slow method. To get a random on the order used RNGCryptoServiceProvider
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source)
+        {
+            var list = source.ToList();
+            list.ShuffleThis();
+            return list;
+        }
+
+        /// <summary>
+        /// Shuffle elements of list in very good random order. Very slow method. To get a random on the order used RNGCryptoServiceProvider
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        public static void ShuffleThis<T>(this IList<T> list)
+        {
+            var provider = new RNGCryptoServiceProvider();
+            var n = list.Count;
+            while (n > 1)
+            {
+                var box = new byte[1];
+                do
+                {
+                    provider.GetBytes(box);
+                }
+                while (!(box[0] < n * (Byte.MaxValue / n)));
+                var k = (box[0] % n);
+
+                n--;
+
+                var value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+
+        /// <summary>
+        /// Shuffle elements in random order. To get a random on the order used Random class.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> FastShuffle<T>(this IEnumerable<T> source)
+        {
+            var rnd = new Random();
+            return source.OrderBy(item => rnd.Next());
+        }
+
+        /// <summary>
+        /// Shuffle elements of list in random order. To get a random on the order used Random class
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        public static void FastShuffleThis<T>(this IList<T> list)
+        {
+            var rng = new Random();
+            var n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                var k = rng.Next(n + 1);
+                var value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+    }
+}
---
+++ src/BuildingBlocks.Common/NaturalComparer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace BuildingBlocks.Common
+{
+    public class NaturalComparer : Comparer<string>
+    {
+        private static readonly Regex _splitRegex;
+
+        static NaturalComparer()
+        {
+            _splitRegex = new Regex("([0-9]+)", RegexOptions.Compiled);
+        }
+
+        public override int Compare(string x, string y)
+        {
+            if (x == y)
+                return 0;
+
+            var xParts = _splitRegex.Split(CutSpaces(x));
+            var yParts = _splitRegex.Split(CutSpaces(y));
+            for (var i = 0; i < xParts.Length && i < yParts.Length; i++)
+            {
+                if (xParts[i] != yParts[i])
+                {
+                    return PartCompare(xParts[i], yParts[i]);
+                }
+            }
+
+            return yParts.Length.CompareTo(xParts.Length);
+        }
+
+        private static int PartCompare(string left, string right)
+        {
+            int x, y;
+            if (!int.TryParse(left, out x))
+            {
+                return string.Compare(left, right, StringComparison.Ordinal);
+            }
+            if (!int.TryParse(right, out y))
+            {
+                return string.Compare(left, right, StringComparison.Ordinal);
+            }
+
+            return x.CompareTo(y);
+        }
+
+        private static string CutSpaces(string x)
+        {
+            return x.Replace(" ", "");
+        }
+    }
+}
